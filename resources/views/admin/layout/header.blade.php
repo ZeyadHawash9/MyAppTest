a7c1739fd59504f55df38dbb1b1ad9f2b3097817
zeyadaverotech <98884466+zeyadaverotech@users.noreply.github.com>
2024-05-01 14:39:22 +0300

    add logout

--- resources/views/admin/layout/header.blade.php
+++ resources/views/admin/layout/header.blade.php
@@ -5887,8 +5887,9 @@
                         <!--end::Menu item-->
                         <!--begin::Menu item-->
                         <div class="menu-item px-5">
-                            <a href="../../demo1/dist/authentication/layouts/corporate/sign-in.html"
+                            <a href="{{ route('dashboard.logout')}}"
                                 class="menu-link px-5">Sign Out</a>
+
                         </div>
                         <!--end::Menu item-->
                     </div>
